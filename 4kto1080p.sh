7e050694beb9a9178630f63b592f0147e5c4f7e4
Sean Chapel <seoushi@gmail.com>
2016-05-23 22:39:54 -0700

    added 4kto1080p script

---
+++ 4kto1080p.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if ! [[ -d $2 ]]; then
+    echo "bad output directory"
+    exit 2
+fi
+
+function process_file {
+    file=${1}
+    base=${file%.*}
+    name=${base##*/}
+    ffmpeg -i $1 -vf scale=1920:1080 -c:v libx264 -crf 20 -preset slow $2/${name}"_1080.mp4"
+}
+
+if [[ -d $1 ]]; then
+    for file in `ls $1 | grep -i -e .mp4`
+    do
+	process_file "$1$file" $2
+    done
+elif [[ -f $1 ]]; then
+    process_file $1 $2
+else
+    echo "invalid argument"
+    exit 1
+fi
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# utils
+# Utils
+A random collection of utilies/scripts I've written
+
+# 4kto1080p
+a script to convert 4k video to 1080p.
+4kto1080p.sh input_file_or_directory output_directory
